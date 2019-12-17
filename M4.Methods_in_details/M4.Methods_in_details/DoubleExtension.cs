0fc342fafbe5cfccf82e60cfead9335b3a865961
Kate <ekaterina.g2015@mail.ru>
2019-12-18 01:21:01 +0400

    A part of first task was implemented

---
+++ M4.Methods_in_details/M4.Methods_in_details/DoubleExtension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M4.Methods_in_details
+{
+    public static class DoubleExtension
+    {
+        public static string ToIEEE754Format(this double number)
+        {
+            var stringNumber = new StringBuilder();
+
+            stringNumber.Append(number > 0 ? "0" : "1");//записываем знак числа
+
+            var part = Math.Abs(number).ToString().Split(',');
+            var firstPart = Convert.ToString(Convert.ToInt32(part[0]),2); //двоичное представление части числа до ,
+            var secondPart = Convert.ToInt32(part[1])*Math.Pow(10,-part[1].Length);
+            var b = 0.5;
+            var sum = 0.0;
+            var degree = 1;
+            var temp = 0.0;
+            var binarySecondPart = new List<byte>();
+            while (true)
+            {
+                if (Math.Abs(sum - secondPart) < 0.0000000001)
+                    break;
+                temp = Math.Pow(b, degree);
+                if (sum + temp <= secondPart)
+                {
+                    sum += temp;
+                    binarySecondPart.Add(1);
+                }
+                else
+                    binarySecondPart.Add(0);
+                degree++;
+            }
+            var binSPart = new StringBuilder();
+            foreach(var s in binarySecondPart)
+                binSPart.Append(s == 0 ? "0" : "1");
+
+            var fPMant = new StringBuilder();
+            fPMant.Append(firstPart.Substring(firstPart.IndexOf('1') + 1));//какой должна быть длина и как её задать?
+            while (fPMant.Length < 7)
+                fPMant.Append(0);
+
+            var mantissa = fPMant + binSPart;
+            var exp = 128 - 1 + fPMant.Length;
+            stringNumber.Append(exp);
+            stringNumber.Append(mantissa);
+            return stringNumber.ToString();
+        }
+    }
+}
---
+++ M4.Methods_in_details/M4.Methods_in_details/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M4.Methods_in_details
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleExtension.ToIEEE754Format(185.4375);
+        }
+    }
+}
