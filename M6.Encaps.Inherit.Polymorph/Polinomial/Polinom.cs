31cd48dbb880cdfd0ee3a9e10090454b7acd409d
Kate <ekaterina.g2015@mail.ru>
2020-02-03 21:30:33 +0400

    addition and subtraction operations for polynomials have been rewritten. Added tests and multiplication operation

--- M6.Encaps.Inherit.Polymorph/Polinomial/Polinom.cs
+++ M6.Encaps.Inherit.Polymorph/Polinomial/Polinom.cs
@@ -10,7 +10,7 @@ namespace Polinomial
 
         public Polinom(Dictionary<int, double> coef)
         {
-            this.Coefficiencts = coef;
+            Coefficiencts = coef;
         }
 
         public Polinom()
@@ -27,11 +27,10 @@ namespace Polinomial
                 return false;
             if (Coefficiencts.Count != p.Coefficiencts.Count)
                 return false;
-            for (var i = 0; i < Coefficiencts.Count; i++)
-            {
-                if (Coefficiencts[i] != p.Coefficiencts[i])
+
+            foreach(var key in Coefficiencts.Keys)
+                if(!p.Coefficiencts.ContainsKey(key))
                     return false;
-            }
             return true;
         }
 
@@ -52,30 +51,22 @@ namespace Polinomial
             if (p1.Coefficiencts.Keys.Min() < 0 || p2.Coefficiencts.Keys.Min() < 0)
                 throw new ArgumentException("Keys must be positive or zero");
 
-            var maxDegree = Math.Max(p1.Coefficiencts.Keys.Max(), p2.Coefficiencts.Keys.Max());
             var p3 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
-            for (var i = 0; i <= maxDegree; i++)
-            {
-                if (i <= p1.Coefficiencts.Keys.Max())
-                {
-                    if (p1.Coefficiencts.ContainsKey(i))
-                        if (p3.Coefficiencts.ContainsKey(i))
-                            p3.Coefficiencts[i] += p1.Coefficiencts[i];
-                        else p3.Coefficiencts.Add(i, p1.Coefficiencts[i]);
-                }
 
-                if (i <= p2.Coefficiencts.Keys.Max())
-                {
-                    if (p2.Coefficiencts.ContainsKey(i))
-                        if (p3.Coefficiencts.ContainsKey(i))
-                            p3.Coefficiencts[i] += p2.Coefficiencts[i];
-                        else p3.Coefficiencts.Add(i, p2.Coefficiencts[i]);
-                }
+            foreach (var key in p1.Coefficiencts.Keys)
+            {
+                if (p3.Coefficiencts.ContainsKey(key))
+                    p3.Coefficiencts[key] += p1.Coefficiencts[key];
+                else p3.Coefficiencts.Add(key, p1.Coefficiencts[key]);
             }
-            foreach (var c in p3.Coefficiencts)
-                if (c.Value == 0)
-                    p3.Coefficiencts.Remove(c.Key);
-            return p3;
+            foreach (var key in p2.Coefficiencts.Keys)
+            {
+                if (p3.Coefficiencts.ContainsKey(key))
+                    p3.Coefficiencts[key] += p2.Coefficiencts[key];
+                else p3.Coefficiencts.Add(key, p2.Coefficiencts[key]);
+            }
+
+            return RemoveZeroElements(p3);
         }
 
         public static Polinom operator -(Polinom p1, Polinom p2)
@@ -87,50 +78,58 @@ namespace Polinomial
                 return p2;
             }
             else if (p1.Coefficiencts.Count != 0 && p2.Coefficiencts.Count == 0)
-            {
-                for (var i = 0; i < p1.Coefficiencts.Count; i++)
-                    p1.Coefficiencts[i] = -p1.Coefficiencts[i];
-                return p1;
-            }
+                return p1;            
             else if (p1.Coefficiencts.Count == 0 && p2.Coefficiencts.Count == 0)
                 return p1;
 
             if (p1.Coefficiencts.Keys.Min() < 0 || p2.Coefficiencts.Keys.Min() < 0)
                 throw new ArgumentException("Keys must be positive or zero");
 
-            var maxDegree = Math.Max(p1.Coefficiencts.Keys.Max(), p2.Coefficiencts.Keys.Max());
             var p3 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
-            for (var i = 0; i <= maxDegree; i++)
+            foreach (var key in p1.Coefficiencts.Keys)
             {
-                if (i <= p1.Coefficiencts.Keys.Max())
-                {
-                    if (p1.Coefficiencts.ContainsKey(i))
-                        if (p3.Coefficiencts.ContainsKey(i))
-                            p3.Coefficiencts[i] -= p1.Coefficiencts[i];
-                        else p3.Coefficiencts.Add(i, p1.Coefficiencts[i]);
-                }
-
-                if (i <= p2.Coefficiencts.Keys.Max())
-                {
-                    if (p2.Coefficiencts.ContainsKey(i))
-                        if (p3.Coefficiencts.ContainsKey(i))
-                            p3.Coefficiencts[i] -= p2.Coefficiencts[i];
-                        else p3.Coefficiencts.Add(i, -p2.Coefficiencts[i]);
-                }
+                if (p3.Coefficiencts.ContainsKey(key))
+                    p3.Coefficiencts[key] -= p1.Coefficiencts[key];
+                else p3.Coefficiencts.Add(key, p1.Coefficiencts[key]);
             }
-            foreach (var c in p3.Coefficiencts)
-                if (c.Value == 0)
-                    p3.Coefficiencts.Remove(c.Key);
-            return p3;
+            foreach (var key in p2.Coefficiencts.Keys)
+            {
+                if (p3.Coefficiencts.ContainsKey(key))
+                    p3.Coefficiencts[key] -= p2.Coefficiencts[key];
+                else p3.Coefficiencts.Add(key, -p2.Coefficiencts[key]);
+            }
+
+            return RemoveZeroElements(p3);
         }
 
         public static Polinom operator *(Polinom p1, Polinom p2)
         {
-            var maxDegree = p1.Coefficiencts.Keys.Max() + p2.Coefficiencts.Keys.Max();
+            if (p1.Coefficiencts.Count == 0 || p2.Coefficiencts.Count == 0)
+                return new Polinom() { Coefficiencts = new Dictionary<int, double>() };
+
+            if (p1.Coefficiencts.Keys.Min() < 0 || p2.Coefficiencts.Keys.Min() < 0)
+                throw new ArgumentException("Keys must be positive or zero");
+
             var p3 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
 
-            p3.Coefficiencts[0] = p1.Coefficiencts[0] * p2.Coefficiencts[0];
-            return p3;
+            foreach (var key1 in p1.Coefficiencts.Keys)
+                foreach (var key2 in p2.Coefficiencts.Keys)
+                    if (p3.Coefficiencts.ContainsKey(key1 + key2))
+                        p3.Coefficiencts[key1 + key2] += p1.Coefficiencts[key1] * p2.Coefficiencts[key2];
+                    else p3.Coefficiencts.Add(key1 + key2, p1.Coefficiencts[key1] * p2.Coefficiencts[key2]);
+               
+            return RemoveZeroElements(p3);
+        }
+
+        private static Polinom RemoveZeroElements(Polinom p)
+        {
+            var count = p.Coefficiencts.Count;
+            for (var i = 0; i < count; i++)
+            {
+                if (p.Coefficiencts.ContainsKey(i) && p.Coefficiencts[i] == 0)
+                    p.Coefficiencts.Remove(i);
+            }
+            return p;            
         }
     }
 }
--- M6.Encaps.Inherit.Polymorph/PolinomialTests/PolinomTests.cs
+++ M6.Encaps.Inherit.Polymorph/PolinomialTests/PolinomTests.cs
@@ -34,6 +34,23 @@ namespace PolinomialTests
         }
 
         [TestMethod]
+        public void PolinomSumTest4()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 0 }, { 1, 0 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, 4 }, { 3, 5 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 3 }, { 2, 7 }, { 3, 5 } } }, p1 + p2);
+        }
+
+        [TestMethod]
+        public void PolinomSumTest5()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 0 }, { 1, 0}, { 2, 0 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, 4 }, { 3, 5 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, 4 }, { 3, 5 } } }, p1 + p2);
+        }
+
+
+        [TestMethod]
         public void PolinomSumWithEmptyTest1()
         {
             var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double>()};
@@ -89,6 +106,30 @@ namespace PolinomialTests
         }
 
         [TestMethod]
+        public void PolinomSubtractionTest4()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 0 }, { 1, 0 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, 0 }, { 3, 5 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, -2 }, { 2, 3 }, { 3, -5 } } }, p1 - p2);
+        }
+
+        [TestMethod]
+        public void PolinomSubtractionTest5()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 0 }, { 1, 0 }, { 2, 0 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, 4 }, { 3, 5 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, -2 }, { 2, -4 }, { 3, -5 } } }, p1 - p2);
+        }
+
+        [TestMethod]
+        public void PolinomSubExceptionTest1()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double>() { { 0, 1 }, { -1, 2 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double>() { { 0, 1 }, { 1, 2 }, { 2, 3 } } };
+            Assert.ThrowsException<ArgumentException>(() => p1 - p2);
+        }
+
+        [TestMethod]
         public void PolinomSubWithEmptyTest1()
         {
             var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
@@ -110,6 +151,79 @@ namespace PolinomialTests
             var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
             var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
             Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double>() }, p1 - p2);
+        }
+
+        [TestMethod]
+        public void PolinomMultiplicationTest1()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 1 }, { 1, 2 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 1, 1 }, { 2, 4 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 1, 5 }, { 2, 12 }, { 3, 11 }, { 4, 12 } } }, p1 * p2);
+        }
+
+        [TestMethod]
+        public void PolinomMultiplicationTest2()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 1 }, { 1, 0 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, -4 }, { 3, 5 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, 2 }, { 3, 5 }, { 4, -12 }, { 5, 15 } } }, p1 * p2);
+        }
+
+        [TestMethod]
+        public void PolinomMultiplicationTest3()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 1 }, { 1, 2 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, -4 }, { 3, 5 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 1, 4 }, { 2, 2 }, { 3, -3 }, { 4, -2 }, { 5, 15 } } }, p1 * p2);
+        }
+
+        [TestMethod]
+        public void PolinomMultiplicationTest4()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 1, 0 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, 0 }, { 3, 5 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 4 }, { 2, 6 }, { 3, 10 }, { 5, 15 } } }, p1 * p2);
+        }
+
+        [TestMethod]
+        public void PolinomMultiplicationTest5()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 0 }, { 1, 0 }, { 2, 0 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 2, 4 }, { 3, 5 } } };
+            var res = p1 * p2;
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double> () }, res);
+        }
+
+        [TestMethod]
+        public void PolinomMultExceptionTest1()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double>() { { 0, 1 }, { -1, 2 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double>() { { 0, 1 }, { 1, 2 }, { 2, 3 } } };
+            Assert.ThrowsException<ArgumentException>(() => p1 * p2);
+        }
+
+        [TestMethod]
+        public void PolinomMultWithEmptyTest1()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 2 }, { 1, 1 }, { 2, 4 }, { 3, 5 } } };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double>() }, p1 * p2);
+        }
+
+        [TestMethod]
+        public void PolinomMultWithEmptyTest2()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double> { { 0, 1 }, { 1, 2 }, { 2, 3 } } };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double>() }, p1 * p2);
+        }
+
+        [TestMethod]
+        public void PolinomMultWithEmptyTest3()
+        {
+            var p1 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
+            var p2 = new Polinom() { Coefficiencts = new Dictionary<int, double>() };
+            Assert.AreEqual(new Polinom() { Coefficiencts = new Dictionary<int, double>() }, p1 * p2);
         }
     }
 }
