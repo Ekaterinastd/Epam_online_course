1292b630e2b234d0862ba47df1730020c9ac241a
Kate <ekaterina.g2015@mail.ru>
2020-02-12 23:09:10 +0400

    SumBigNumber for negative numbers with tests were aaded

--- M7.Framework_Fundamentals/M7.Framework_Fundamentals/BigNumbers.cs
+++ M7.Framework_Fundamentals/M7.Framework_Fundamentals/BigNumbers.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace M7.Framework_Fundamentals
 {
@@ -37,6 +35,13 @@ namespace M7.Framework_Fundamentals
             var result = new StringBuilder();
             var remainder = 0;
             var sum = 0;
+            var isNegative = false;
+            if (rev_n1.Contains("-") && rev_n2.Contains("-"))
+            {
+                rev_n1 = rev_n1.Substring(0, rev_n1.Length - 1);
+                rev_n2 = rev_n2.Substring(0, rev_n2.Length - 1);
+                isNegative = true;
+            }
 
             for (var i = 0; i < Math.Max(rev_n1.Length, rev_n2.Length); i++)
             {
@@ -56,69 +61,179 @@ namespace M7.Framework_Fundamentals
 
             if (remainder == 1)
                 result.Append(remainder);
-            if (rev_n1.Contains("-") && rev_n2.Contains("-"))
+            if (isNegative)
                 result.Append("-");
             return Reverse(result.ToString());
         }
 
-        private static string SumDifSignsNumbers(string rev_n1, string rev_n2)
+        private static string SumDifSignsNumbers(string n1, string n2)
         {
             var result = new StringBuilder();
             var loan = 0; //заём
             var diff = 0;//разность
-            if (rev_n1.Contains("-"))
-                rev_n1 = rev_n1.Substring(0, rev_n1.Length - 1);
-            if (rev_n2.Contains("-"))
-                rev_n2 = rev_n2.Substring(0, rev_n2.Length - 1);
+            var multiLevel_Loan = false;
+            var rev_n1 = "";
+            var rev_n2 = "";
+            if (n1.Contains("-"))
+                rev_n1 = n1.Substring(0, n1.Length - 1);
+            else rev_n1 = n1;
 
-            if (rev_n1.Length >= rev_n2.Length)//если уменьшаемое (по модулю) больше или равно вычитаемого
+            if (n2.Contains("-"))
+                rev_n2 = n2.Substring(0, n2.Length - 1);
+            else rev_n2 = n2;
+
+            if (rev_n1.Length >= rev_n2.Length)//если уменьшаемое (по длине) больше или равно вычитаемого
             {
                 for (var i = 0; i < Math.Max(rev_n1.Length, rev_n2.Length); i++)
                 {
                     if (i < rev_n2.Length)
                     {
                         diff = rev_n1[i] - rev_n2[i] - loan;
-                        if (diff < 0)
+
+                        if (multiLevel_Loan)//если нельзя занять у следующего разряда (он = 0)
                         {
-                            diff += 10;
-                            loan = 1;
+                            if (diff < 0)
+                            {
+                                diff += 9;
+                                if (i < rev_n1.Length - 1 && rev_n1[i + 1] > 0)
+                                {
+                                    loan = 1;
+                                    multiLevel_Loan = false;
+                                }
+                                else loan = 0;
+                            }
+                            else loan = 0;
                         }
-                        else loan = 0;
+                        else
+                        {
+                            if (diff < 0)
+                            {
+                                diff += 10;
+                                loan = 1;
+                                if (i < rev_n1.Length - 1 && rev_n1[i + 1] == '0')
+                                    multiLevel_Loan = true;
+                            }
+                            else loan = 0;
+                        }
+
                         result.Append(diff);
                     }
                     else if (i == rev_n2.Length && loan == 1)
-                        if(rev_n1[i]!=0)
+                        if (rev_n1[i] != '0')
                             result.Append(rev_n1[i] - 1 - '0');
                         else
                         {
+                            multiLevel_Loan = true;
+                            if (multiLevel_Loan)//если нельзя занять у следующего разряда (он = 0)
+                            {
+                                if (i < rev_n1.Length - 1 && rev_n1[i + 1] != '0')
+                                {
+                                    loan = 1;
+                                    multiLevel_Loan = false;
+                                }
+                                else loan = 0;
+                                result.Append(9);
 
+                            }
                         }
-                    else result.Append(rev_n1[i] - '0');
+                    else if (multiLevel_Loan)
+                    {
+                        if (i == rev_n1.Length - 2 && rev_n1[i + 1] != '0')
+                        {
+                            loan = 1;
+                            multiLevel_Loan = false;
+                        }
+                        result.Append(9);
+                    }
+
+                    else result.Append(rev_n1[i] - '0' - loan);
                 }
             }
-            else
+            else//если уменьшаемое (по длине) меньше вычитаемого
             {
                 for (var i = 0; i < Math.Max(rev_n1.Length, rev_n2.Length); i++)
                 {
                     if (i < rev_n1.Length)
                     {
                         diff = rev_n2[i] - rev_n1[i] - loan;
-                        if (diff < 0)
+
+                        if (multiLevel_Loan)//если нельзя занять у следующего разряда (он = 0)
                         {
-                            diff += 10;
-                            loan = 1;
+                            if (diff < 0)
+                            {
+                                diff += 9;
+                                if (i < rev_n2.Length - 1 && rev_n2[i + 1] > 0)
+                                {
+                                    loan = 1;
+                                    multiLevel_Loan = false;
+                                }
+                                else loan = 0;
+                            }
+                            else loan = 0;
                         }
-                        else loan = 0;
+                        else
+                        {
+                            if (diff < 0)
+                            {
+                                diff += 10;
+                                loan = 1;
+                                if (i < rev_n2.Length - 1 && rev_n2[i + 1] == '0')
+                                    multiLevel_Loan = true;
+                            }
+                            else loan = 0;
+                        }
+
                         result.Append(diff);
                     }
-                    if (i == rev_n1.Length && loan == 1)
-                        result.Append(rev_n2[i] - 1 - '0');
-                    else result.Append(rev_n2[i] - '0');
+                    else if (i == rev_n1.Length && loan == 1)
+                        if (rev_n2[i] != '0')
+                            result.Append(rev_n2[i] - 1 - '0');
+                        else
+                        {
+                            multiLevel_Loan = true;
+                            if (multiLevel_Loan)//если нельзя занять у следующего разряда (он = 0)
+                            {
+                                if (i < rev_n2.Length - 1 && rev_n2[i + 1] != '0')
+                                {
+                                    loan = 1;
+                                    multiLevel_Loan = false;
+                                }
+                                else loan = 0;
+                                result.Append(9);
+                            }
+                        }
+                    else if (multiLevel_Loan)
+                    {
+                        if (i == rev_n2.Length - 2 && rev_n2[i + 1] != '0')
+                        {
+                            loan = 1;
+                            multiLevel_Loan = false;
+                        }
+                        result.Append(9);
+                    }
+
+                    else result.Append(rev_n2[i] - '0' - loan);
                 }
             }
 
-            if (result[result.Length - 1] == 0)
+            while (result.Length > 0 && result[result.Length - 1] == '0')
+            {
                 result.Remove(result.Length - 1, 1);
+            }
+            if (result.Length == 0)
+                return "0";
+
+            if ((n1.Length > n2.Length && n1.Contains('-')) || (n1.Length < n2.Length && n2.Contains('-')))
+                result.Append('-');
+            if (rev_n1.Length == rev_n2.Length)
+            {
+                for (var j = rev_n1.Length; j <= 0; j--)
+                    if (rev_n1[j] - '0' < rev_n2[j] - '0')
+                    {
+                        result.Append('-');
+                        break;
+                    }
+            }
             return Reverse(result.ToString());
         }
     }
