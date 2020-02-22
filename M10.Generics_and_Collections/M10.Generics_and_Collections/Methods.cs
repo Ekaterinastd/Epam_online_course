b1a511f789a4a1ab3db95053827bf53c4aeb18a6
Kate <ekaterina.g2015@mail.ru>
2020-02-23 00:44:43 +0400

    task 1,2,8 were done

---
+++ M10.Generics_and_Collections/M10.Generics_and_Collections/Methods.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace M10.Generics_and_Collections
+{
+    public class BinarySearch
+    {
+        public static int BinarySearchGeneric<T>(ICollection<T> collection, T element) where T : IComparable
+        {
+            for (var i = 0; i < collection.Count - 1; i++)
+            {
+                if (collection.ElementAt(i).CompareTo(collection.ElementAt(i + 1)) > 0)
+                    throw new ArgumentException("Collection must be sorted");
+            }
+            var left = 0;
+            var right = collection.Count - 1;
+            while (left < right)
+            {
+                var middle = (right + left) / 2;
+                if (collection.ElementAt(middle).CompareTo(element) >= 0)
+                    right = middle;
+                else left = middle + 1;
+            }
+            if (collection.ElementAt(right).CompareTo(element) == 0)
+                return right;
+
+            return -1;
+        }
+    }
+
+    public class WordsFrequency
+    {
+        public static Dictionary<string, double> GetWordsFrequency(string text)
+        {
+            var words = text.ToLower().Split(new char[] { '.', '?', '!', ',', ':', ';', ' ' },StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length == 0)
+                throw new ArgumentException("String is empty");
+
+            var frequency = new Dictionary<string, double>();
+
+            foreach(var word in words)
+            {                
+                if (!frequency.ContainsKey(word))
+                    frequency.Add(word, 1.0 / words.Length);
+                else frequency[word] += 1.0 / words.Length;
+            }
+            return frequency;
+        }
+    }
+
+    public class ReversePolishNotation
+    {
+        public static int GetReversePolishNotation(string str)
+        {            
+            var operations = new Dictionary<string, Func<int, int, int>>();
+            operations.Add("+", (y, x) => x + y);
+            operations.Add("-", (y, x) => x - y);
+            operations.Add("*", (y, x) => x * y);
+            operations.Add("/", (y, x) => x / y);
+
+            var stack = new Stack<int>();
+
+            var list = str.Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries);
+            if (list.Length == 0)
+                return 0;
+            var res = 0;
+            foreach (var e in list)
+            {
+                if (int.TryParse(e.ToString(), out res))
+                    stack.Push(res);
+                else if (operations.ContainsKey(e))
+                    stack.Push(operations[e](stack.Pop(), stack.Pop()));
+                else throw new ArgumentException("String must contain spaces");
+            }
+            return stack.Pop();
+        }
+    }
+
+    public class FibonacciNumbers
+    {
+
+    }
+}
---
+++ M10.Generics_and_Collections/UnitTestProject1/Tests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using M10.Generics_and_Collections;
+using System.Collections.Generic;
+
+namespace TestMethods
+{
+    [TestClass]
+    public class BinarySearchGenericTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(BinarySearch.BinarySearchGeneric(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 }, 6), 5);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(BinarySearch.BinarySearchGeneric(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 }, 10), -1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(BinarySearch.BinarySearchGeneric(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 }, 1), 0);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(BinarySearch.BinarySearchGeneric(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 }, 8), 7);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(BinarySearch.BinarySearchGeneric(new List<int>() { 1, 2, 3, 4, 6, 7, 8 }, 5), -1);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(BinarySearch.BinarySearchGeneric(new int [] { 1, 2, 3, 4, 6, 7, 8 }, 5), -1);
+        }
+    }
+
+    [TestClass]
+    public class GetWordsFrequencyTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CollectionAssert.AreEqual(WordsFrequency.GetWordsFrequency("AA z d v sd aa sd c z aa c"),new Dictionary<string, double>() { ["aa"] = 3.0 / 11, ["z"] = 2.0 / 11,["d"] = 1.0 / 11, ["v"] = 1.0 / 11, ["sd"] = 2.0 / 11, ["c"] = 2.0 / 11 });
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            CollectionAssert.AreEqual(WordsFrequency.GetWordsFrequency("AA, z ! d v sd aa sd c z aa c."), new Dictionary<string, double>() { ["aa"] = 3.0 / 11, ["z"] = 2.0 / 11, ["d"] = 1.0 / 11, ["v"] = 1.0 / 11, ["sd"] = 2.0 / 11, ["c"] = 2.0 / 11 });
+        }
+
+        [TestMethod]
+        public void GetExceptionTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => WordsFrequency.GetWordsFrequency(""));
+        }
+    }
+
+    [TestClass]
+    public class ReversePolishNotationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(ReversePolishNotation.GetReversePolishNotation("5 1 2 + 4 * + 3 -"),14);
+        }
+
+        [TestMethod]
+        public void GetException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => ReversePolishNotation.GetReversePolishNotation("5 1 2+ 4 * + 3 -"));
+        }
+
+        [TestMethod]
+        public void EmptyStringTest()
+        {
+            Assert.AreEqual(ReversePolishNotation.GetReversePolishNotation(""), 0);
+        }
+
+        [TestMethod]
+        public void EmptyStringTest2()
+        {
+            Assert.AreEqual(ReversePolishNotation.GetReversePolishNotation(" "), 0);
+        }
+    }
+ }
