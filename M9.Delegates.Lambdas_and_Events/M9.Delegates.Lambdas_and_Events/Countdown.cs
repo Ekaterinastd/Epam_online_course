c4e2c8be3e50c1a3eb33efce2aea6640a5916ab6
Kate <ekaterina.g2015@mail.ru>
2020-02-25 17:47:43 +0400

    remarks corrected (class Program)

--- M9.Delegates.Lambdas_and_Events/M9.Delegates.Lambdas_and_Events/Countdown.cs
+++ M9.Delegates.Lambdas_and_Events/M9.Delegates.Lambdas_and_Events/Countdown.cs
@@ -33,22 +33,5 @@ namespace M9.Delegates.Lambdas_and_Events
         }
     }
        
-    public class Program
-    {
-        public static void Main()
-        {
-            var s1 = new Subscriber("S1");
-            var s2 = new Subscriber("S2");
-            var s3 = new Subscriber("S3");
-
-            var c = new Countdown() { Interval = 3 };
-            c.Tick += s1.Message;
-            c.Tick += s2.Message;
-            c.Tick += s3.Message;
-            c.Start();
-            c.Tick -= s1.Message;
-            c.Start();
-            Console.ReadKey();
-        }
-    }
+   
 }
